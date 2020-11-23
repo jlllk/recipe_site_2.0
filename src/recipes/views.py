f21a5641846d382029f575717c9dac6e74a2dba3
jlllk <ehyf.py@gmail.com>
2020-11-23 23:53:03 +0300

    Added fixtures

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Recipe
+
+
+def index(request):
+    recipes = Recipe.objects.all()
+    context = {'recipes': recipes}
+    return render(request, 'index.html', context)
